8764024a59f6aef5ab71b92c6802b5d40af721c5
herlanassis <herlanassis@gmail.com>
2016-05-30 13:09:16 -0300

    Preparando para inserir entrada

--- src/br/com/ifrn/ed/Maze.java
+++ src/br/com/ifrn/ed/Maze.java
@@ -2,6 +2,8 @@ package br.com.ifrn.ed;
 
 import br.com.ifrn.ed.Stack.MyStack;
 import java.util.ArrayList;
+import java.io.*;
+import java.util.Scanner;
 
 public class Maze {
 
@@ -14,10 +16,46 @@ public class Maze {
     private final char exitMarker = 'e', entryMarker = 'm', visited = '.';
     private final char passage = '0', wall = '1';
 
-    public Maze() {
+    private InputStreamReader isr;
+    private BufferedReader buffer;   
+    private Scanner optionScanner;            
+    private int option;
+
+    public Maze() throws IOException {
         this.mazeStack = new MyStack<>(100);
         this.entryCell = new Cell();
         this.entryCell = new Cell();
+        
+        System.out.println("Selecione a entrada para o labirinto:"
+                + "\n1 - Teclado\n2 - arquivo");
+        do{
+            option = optionScanner.nextInt();
+        }while(option !=1 || option !=2);
+        
+        switch (option){
+            case 1:
+                break;
+            case 2:
+                break;
+        }
+        
+        System.out.println("Digite um labirinto retangular utilizando os seguintes comandos: "
+                + "\nm - entrada\ne - saida\n1 - parede\n0 - passagem\n "
+                + "\nPara finalizar digite Ctrl-d:");
+        
+        isr = new InputStreamReader(System.in);
+        buffer = new BufferedReader(isr);        
+        try {
+            String str = buffer.readLine();
+            while (str != null) {
+                str = buffer.readLine();
+            }
+            buffer.close();
+            System.out.println(str);
+        } catch (EOFException eofe) {
+
+        }
+
     }
 
     @Override
@@ -31,5 +69,5 @@ public class Maze {
 
     public void exitMaze() {
 
-    }        
+    }
 }
--- src/br/com/ifrn/ed/MazeRun.java
+++ src/br/com/ifrn/ed/MazeRun.java
@@ -1,7 +1,9 @@
 package br.com.ifrn.ed;
 
+import java.io.IOException;
+
 public class MazeRun {
-    public static void main(String[] args) {
-        new Maze().exitMaze();
+    public static void main(String[] args) throws IOException {
+        Maze maze = new Maze();
     }
 }
